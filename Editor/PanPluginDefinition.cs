9ff6010d28850eb6ce34863eec8525990e09e796
pandravrc <162582491+pandravrc@users.noreply.github.com>
2025-01-25 17:20:28 +0900

    asmdef定義

---
+++ Editor/PanPluginDefinition.cs
@@ -0,0 +1,26 @@
+﻿using com.github.pandrabox.flatsplus.editor;
+using com.github.pandrabox.pandravase.editor;
+using nadena.dev.ndmf;
+using nadena.dev.ndmf.fluent;
+using static com.github.pandrabox.pandravase.runtime.Util;
+
+[assembly: ExportsPlugin(typeof(PanPluginDefinition))]
+
+namespace com.github.pandrabox.flatsplus.editor
+{
+    internal class PanPluginDefinition : Plugin<PanPluginDefinition>
+    {
+        public override string DisplayName => "FlatsPlus";
+        public override string QualifiedName => "com.github.pandrabox.flatsplus";
+
+        protected override void Configure()
+        {
+#if PANDRADBG
+            SetDebugMode(true);
+#endif
+            Sequence seq;
+            seq = InPhase(BuildPhase.Transforming).BeforePlugin("nadena.dev.modular-avatar");
+            seq.Run(MoverPass.Instance);
+        }
+    }
+}
